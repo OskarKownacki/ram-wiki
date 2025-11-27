23939a77e4079e7f34a09353d7f48e3c12d1f1c1
Oskar Kownacki <legromp123@gmail.com>
2025-11-27 07:48:26 +0100

    Added searchbar

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,3 +1,6 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="mx-[15%] mt-8 grid grid-cols-12 gap-16 p-4">
+    <div class="col-span-10 col-start-2 flex flex-row">
+        <input type="text" placeholder="Search RAMs..." class="w-full p-3 h-full rounded-md rounded-r-none text-black bg-slate-200" />
+        <button class="bg-blue-700 text-white rounded-md p-3 h-full rounded-l-none">Search</button>
+    </div>
 </div>
