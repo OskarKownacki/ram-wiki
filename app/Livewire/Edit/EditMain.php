92be1f38c54362661043459df114c15d58120a9a
Oskar Kownacki <legromp123@gmail.com>
2025-11-27 17:58:34 +0100

    Refactor edit main component: implement tab functionality and remove unused EditTabsContainer

--- app/Livewire/Edit/EditMain.php
+++ app/Livewire/Edit/EditMain.php
@@ -6,8 +6,13 @@ use Livewire\Component;
 
 class EditMain extends Component
 {
+    public int $selectedTabId = 1;
     public function render()
     {
         return view('livewire.edit.edit-main');
+    }
+    public function setTab($tabId)
+    {
+        $this->selectedTabId = $tabId;
     }
 }
--- resources/views/livewire/edit/edit-main.blade.php
+++ resources/views/livewire/edit/edit-main.blade.php
@@ -1,3 +1,69 @@
-<div>
-<livewire:edit.edit-tabs-container />
+<div class="mx-[15%] mt-8 grid grid-cols-12 gap-16 p-4">
+    <button class="col-span-2 col-start-1 bg-emerald-600 rounded-md p-2 flex items-center justify-between">CSV Import
+        @svg('heroicon-o-table-cells', 'w-6 h-6', ['style' => 'color: #ffff'])</button>
+    <div class="col-span-12  min-h-[60vh] bg-zinc-600 rounded-xl">
+        <div class="grid grid-cols-6  mt-2 p-4">
+            <button @class([
+                'text-3xl',
+                'col-span-1',
+                'rounded-md',
+                'hover:bg-zinc-500' => $selectedTabId !== 1,
+                'font-bold border-b-2 border-emerald-500' => $selectedTabId === 1,
+            ]) wire:click="setTab(1)">RAM</button>
+            <button @class([
+                'text-3xl',
+                'col-span-1',
+                'rounded-md',
+                'hover:bg-zinc-500' => $selectedTabId !== 2,
+                'font-bold border-b-2 border-emerald-500' => $selectedTabId === 2,
+            ]) wire:click="setTab(2)">Server</button>
+            <button @class([
+                'text-3xl',
+                'col-span-1',
+                'rounded-md',
+                'hover:bg-zinc-500' => $selectedTabId !== 3,
+                'font-bold border-b-2 border-emerald-500' => $selectedTabId === 3,
+            ]) wire:click="setTab(3)">Trait</button>
+        </div>
+        <hr>
+        <div>
+            @if ($selectedTabId === 1)
+                <form class="grid grid-cols-12 gap-4 p-4 grid-rows-6" id="ram-form">
+                    <div class="col-span-3 row-start-1 flex flex-col">
+                        <label class="text-white text-sm font-bold after:ml-0.5 after:text-red-500 after:content-['*']"
+                            for="ProductCode">Producer code</label>
+                        <input type="text" name="ProductCode"
+                            class="border-2 rounded-md border-emerald-500 placeholder:text-gray-300 p-2 focus:outline-none "
+                            placeholder="Product Code" />
+                    </div>
+                    <div class="col-span-3 row-start-1 col-start-4 flex flex-col">
+                        <label class="text-white text-sm font-bold after:ml-0.5 after:text-red-500 after:content-['*']"
+                            for="TraitId">Trait ID</label>
+                        <input type="number" name="TraitId"
+                            class="border-2 rounded-md border-emerald-500 placeholder:text-gray-300 p-2 focus:outline-none"
+                            placeholder="Trait id" />
+                    </div>
+                    <div class="col-span-6 row-start-2 flex flex-col">
+                        <label class="text-white text-sm font-bold after:ml-0.5 after:text-red-500 after:content-['*']"
+                            for="Description">Description</label>
+                        <input type="text" name="Description"
+                            class="border-2 rounded-md border-emerald-500 placeholder:text-gray-300 p-2 focus:outline-none"
+                            placeholder="Description" />
+                    </div>
+                    <div class="col-span-6 row-start-3 row-span-3 flex flex-col">
+                        <label class="text-white text-sm font-bold after:ml-0.5 after:text-red-500 after:content-['*']"
+                            for="Manufacturer">Manufacturer</label>
+                        <textarea name="Manufacturer"
+                            class="border-2 rounded-md border-emerald-500 placeholder:text-gray-300 p-2 focus:outline-none"
+                            placeholder="Manufacturer" form="ram-form"> </textarea>
+                    </div>
+                </form>
+            @elseif($selectedTabId === 2)
+
+            @elseif($selectedTabId === 3)
+            @endif
+
+        </div>
+    </div>
+
 </div>
