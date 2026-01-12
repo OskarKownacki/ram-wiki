fcb92edac7acad8cd191d1fd5233674816ace3b5
Oskar Kownacki <legromp123@gmail.com>
2026-01-12 13:38:09 +0100

    refactor: remove unused CSV file validation attribute from EditMain component

--- app/Livewire/Edit/EditMain.php
+++ app/Livewire/Edit/EditMain.php
@@ -2,16 +2,12 @@
 
 namespace App\Livewire\Edit;
 
-use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
 class EditMain extends Component
 {
     use WithFileUploads;
-
-    #[Validate('file', 'mimes:csv', 'max:10240')]
-    public $csv_file;
 
     public int $selectedTabId = 1;
 
