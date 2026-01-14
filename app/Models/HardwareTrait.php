b2a6d273b244cb2af019df3601624afadb00684d
Oskar Kownacki <legromp123@gmail.com>
2026-01-14 20:12:40 +0100

    fix: refactor boolean casting for ecc_support and ecc_registered in HardwareTrait model

--- app/Models/HardwareTrait.php
+++ app/Models/HardwareTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class HardwareTrait extends Model
@@ -27,12 +28,20 @@ class HardwareTrait extends Model
         'manufacturer',
     ];
 
-    protected function casts(): array
+    protected function eccSupport(): Attribute
     {
-        return [
-            'ecc_support'    => 'boolean',
-            'ecc_registered' => 'boolean',
-        ];
+        return Attribute::make(
+            get: fn ($value) => (bool) $value,
+            set: fn ($value) => (bool) $value,
+        );
+    }
+
+    protected function eccRegistered(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => (bool) $value,
+            set: fn ($value) => (bool) $value,
+        );
     }
 
     public function rams()
